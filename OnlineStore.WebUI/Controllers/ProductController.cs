24ea60f2560a7851e2c804e21b6fb38f2d0a1dde
David Nemeth <dt-dave@hotmail.com>
2015-10-21 20:50:56 +0200

    Added Paging

--- OnlineStore.WebUI/Controllers/ProductController.cs
+++ OnlineStore.WebUI/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using OnlineStore.Domain.Abstract;
+using OnlineStore.WebUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,13 +11,27 @@ namespace OnlineStore.WebUI.Controllers
     public class ProductController : Controller
     {
         private readonly IProductRepository repository;
+        public int PageSize = 4;
         public ProductController(IProductRepository repo)
         {
             repository = repo;
         }
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = repository.Products
+                .OrderBy(p => p.ProductId)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+            return View(model);
         }
     }
 }
