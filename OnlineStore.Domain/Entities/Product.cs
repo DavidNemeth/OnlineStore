66b369af9ad4ccd20e74538da3078ef8559f84c3
David Nemeth <dt-dave@hotmail.com>
2015-10-22 17:37:37 +0200

    Updated layout

--- OnlineStore.Domain/Entities/Product.cs
+++ OnlineStore.Domain/Entities/Product.cs
@@ -13,5 +13,6 @@ namespace OnlineStore.Domain.Entities
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string Category { get; set; }
+        public string Image { get; set; }
     }
 }
