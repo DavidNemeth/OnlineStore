b0d36b9ebb1f603ec9d14878091651e71730f4a5
David Nemeth <dt-dave@hotmail.com>
2015-10-24 04:15:57 +0200

    Layout update

--- OnlineStore.WebUI/Controllers/CartController.cs
+++ OnlineStore.WebUI/Controllers/CartController.cs
@@ -30,7 +30,12 @@ namespace OnlineStore.WebUI.Controllers
         {
             return PartialView(cart);
         }
-        
+
+        public ViewResult Checkout()
+        {
+            return View(new ShippingDetails());
+        }
+        [HttpPost]
         public ViewResult Checkout(Cart cart, ShippingDetails shippingDetails)
         {
             if (cart.Lines.Count() == 0)
