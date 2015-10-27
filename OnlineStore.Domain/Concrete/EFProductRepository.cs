df385e3c51541c58123bc2be2bcbc91464378c13
David Nemeth <dt-dave@hotmail.com>
2015-10-27 04:15:24 +0100

    Implemented Admin Page

--- OnlineStore.Domain/Concrete/EFProductRepository.cs
+++ OnlineStore.Domain/Concrete/EFProductRepository.cs
@@ -1,4 +1,5 @@
 ﻿using OnlineStore.Domain.Abstract;
+using OnlineStore.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,38 @@ namespace OnlineStore.Domain.Concrete
         public IEnumerable<Entities.Product> Products
         {
             get { return context.Products; }
+        }
+
+        public Product DeleteProduct(int productId)
+        {
+            Product dbEntry = context.Products.Find(productId);
+            if (dbEntry != null)
+            {
+                context.Products.Remove(dbEntry);
+                context.SaveChanges();
+            }
+            return dbEntry;
+        }
+
+        public void SaveProduct(Product product)
+        {
+            if (product.ProductId == 0)
+            {
+                context.Products.Add(product);
+            }
+            else
+            {
+                Product dbEntry =
+                    context.Products.Find(product.ProductId);
+                if (dbEntry != null)
+                {
+                    dbEntry.Name = product.Name;
+                    dbEntry.Description = product.Description;
+                    dbEntry.Price = product.Price;
+                    dbEntry.Category = product.Category;
+                };
+            }
+            context.SaveChanges();
         }
     }
 }
